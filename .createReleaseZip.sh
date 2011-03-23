d02bceb10d5cc691949c8b0aab873a7b609bae2b
Andrew Matheny <andrew.j.matheny@gmail.com>
2011-03-22 22:16:15 -0400

    Added automatic revision numbers to .createReleaseZip.sh script

--- .createReleaseZip.sh
+++ .createReleaseZip.sh
@@ -1,2 +1,8 @@
 mkdir releases &> /dev/null
+cp manifest.json manifest.json.bak
+revnum=`git log --oneline | wc -l`
+revnum=`echo $revnum`
+cat manifest.json.bak | sed s/\"version\":\"0\"/\"version\":\"1.1."$revnum"\"/ > manifest.json
 zip -b . releases/Syntaxtic_v`./.getVersion.sh`.zip -r `ls | grep -v releases`
+
+mv manifest.json.bak manifest.json
