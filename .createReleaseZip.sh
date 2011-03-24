ec185796dd5ef6887efaff4d1384735ba3f2a53d
U-descarte\amatheny <amatheny@descarte.(none)>
2011-03-24 08:44:27 -0400

    Removed unneeded permissions

--- .createReleaseZip.sh
+++ .createReleaseZip.sh
@@ -2,7 +2,7 @@ mkdir releases &> /dev/null
 cp manifest.json manifest.json.bak
 revnum=`git log --oneline | wc -l`
 revnum=`echo $revnum`
-cat manifest.json.bak | sed s/\"version\":\"0\"/\"version\":\"1.1."$revnum"\"/ > manifest.json
+cat manifest.json.bak | sed s/\"version\":\"0\"/\"version\":\"2.0."$revnum"\"/ > manifest.json
 zip -b . releases/Syntaxtic_v`./.getVersion.sh`.zip -r `ls | grep -v releases`
 
 mv manifest.json.bak manifest.json
