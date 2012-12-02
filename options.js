7034c14e72248c2094f40d97fe3ba184e9141868
Timo Sand <timo.j.sand@gmail.com>
2012-12-02 02:03:19 +0000

    CHANGED: Refactored JS from html files into separate JS files to accommodate Chrome security errors

---
+++ options.js
@@ -0,0 +1,60 @@
+var settings = chrome.extension.getBackgroundPage().syntaxtic.settings;
+
+var style = document.createElement('link');
+style.type = 'text/css';
+style.id = 'theme-style';
+style.rel = "stylesheet";
+style.href = chrome.extension.getURL("styles/" + settings.theme);
+document.head.appendChild(style);
+
+style = document.createElement('link');
+style.type = 'text/css';
+style.id = 'theme-style';
+style.rel = "stylesheet";
+style.href = chrome.extension.getURL("styles/shCore.css");
+document.head.appendChild(style);
+
+style = document.createElement('link');
+style.type = 'text/css';
+style.id = 'theme-style';
+style.rel = "stylesheet";
+style.href = chrome.extension.getURL("styles/shCore.css");
+document.head.appendChild(style);
+
+var script = document.createElement('script');
+script.src = chrome.extension.getURL("scripts/shCore.js");
+script.type = 'text/javascript';
+document.head.appendChild(script);
+
+script = document.createElement('script');
+script.src = chrome.extension.getURL("scripts/shBrushCSharp.js");
+script.type = 'text/javascript';
+document.head.appendChild(script);
+
+function themeChanged() {
+  var box = document.getElementById('themeSelect');
+  var val = box.options[box.selectedIndex].value;
+  settings.theme = val;
+
+  document.getElementById('theme-style').href = chrome.extension.getURL("styles/" + settings.theme);
+}
+
+function sizeChanged() {
+  var box = document.getElementById('sizeSelect');
+  var val = box.options[box.selectedIndex].value;
+  settings.fontSize = val;
+
+  document.getElementById('textSize').innerHTML = ".syntaxhighlighter {font-size: " + settings.fontSize + " !important;}";
+}
+
+function initOptionsPage() {
+
+  document.getElementById('themeSelect').value = settings.theme;
+  document.getElementById('sizeSelect').value = settings.fontSize;
+
+  style = document.createElement('style');
+  style.type = 'text/css';
+  style.id = 'textSize';
+  document.head.appendChild(style);
+  style.innerHTML = ".syntaxhighlighter {font-size: " + settings.fontSize + " !important;}"
+}
