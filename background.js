dddcc61313bc2f5d8a6ae62a319546343e2dbf16
Timo Sand <timo.j.sand@gmail.com>
2012-12-02 22:55:12 +0000

    CHANGED: Added popup to page action to toggle hilight and gutter

--- background.js
+++ background.js
@@ -1,8 +1,3 @@
-chrome.pageAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(
-      null, {code:"var gutter = document.getElementsByClassName('gutter')[0];if (gutter.style.display == ''){ gutter.style.display='none';} else{ gutter.style.display='';}"});
-});
-
 function SyntaxticSettings() {
 
   var _theme = localStorage["syntaxtic.settings.theme"];
