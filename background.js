979fd6a76297630f83d360bde28e3a6473a25eee
Andrew Matheny <andrew.matheny@listhub.com>
2012-12-04 21:23:07 -0500

    Make popup selections for the current page persist to localstorage

--- background.js
+++ background.js
@@ -1,26 +1,57 @@
 function SyntaxticSettings() {
 
+  if( typeof localStorage["syntaxtic.settings.gutterBlacklist"] === "undefined" ) {
+    localStorage["syntaxtic.settings.gutterBlacklist"] = "[]";
+  }
+
+  if( typeof localStorage["syntaxtic.settings.highlightBlacklist"] === "undefined" ) {
+    localStorage["syntaxtic.settings.highlightBlacklist"] = "[]";
+  }
+
+
   var _theme = localStorage["syntaxtic.settings.theme"];
   var _fontSize = localStorage["syntaxtic.settings.fontSize"];
+  var _gutterBlacklist = JSON.parse(localStorage["syntaxtic.settings.gutterBlacklist"]);
+  var _highlightBlacklist = JSON.parse(localStorage["syntaxtic.settings.highlightBlacklist"]);
 
   var _defaultTheme = "shThemeMidnight.css";
   var _defaultFontSize = "normal";
+  var _defaultGutterBlacklist = [];
+  var _defaultHighlightBlacklist = [];
 
   this.__defineGetter__("theme", function(){
     return localStorage["syntaxtic.settings.theme"] || (_theme || _defaultTheme) ;
-    });
-    this.__defineSetter__("theme", function(val){
+  });
+  this.__defineSetter__("theme", function(val){
     _theme = val;
     localStorage["syntaxtic.settings.theme"] = val;
-    });
+  });
 
   this.__defineGetter__("fontSize", function(){
     return localStorage["syntaxtic.settings.fontSize"] || (_fontSize || _defaultFontSize) ;
-    });
-    this.__defineSetter__("fontSize", function(val){
+  });
+  this.__defineSetter__("fontSize", function(val){
     _fontSize = val;
     localStorage["syntaxtic.settings.fontSize"] = val;
-    });
+  });
+
+  this.__defineGetter__("gutterBlacklist", function(){
+    return JSON.parse(localStorage["syntaxtic.settings.gutterBlacklist"]) || (_gutterBlacklist || _defaultGutterBlacklist) ;
+  });
+  this.__defineSetter__("gutterBlacklist", function(val){
+    _gutterBlacklist = val;
+    localStorage["syntaxtic.settings.gutterBlacklist"] = JSON.stringify(val);
+  });
+
+  this.__defineGetter__("highlightBlacklist", function(){
+    return JSON.parse(localStorage["syntaxtic.settings.highlightBlacklist"]) || (_highlightBlacklist || _defaultHighlightBlacklist) ;
+  });
+  this.__defineSetter__("highlightBlacklist", function(val){
+    _highlightBlacklist = val;
+    localStorage["syntaxtic.settings.highlightBlacklist"] = JSON.stringify(val);
+  });
+
+
 }
 
 var syntaxtic = {
--- popup.js
+++ popup.js
@@ -1,12 +1,36 @@
-function click(e) {
-  chrome.tabs.executeScript(null,
-      {file:"toggle_" + e.target.id + ".js"});
-  window.close();
+var settings = chrome.extension.getBackgroundPage().syntaxtic.settings;
+
+
+function init() {
+ chrome.tabs.getSelected(null, function(tab) {
+   var checkboxes = document.querySelectorAll('.clicker');
+    for (var i = 0; i < checkboxes.length; i++) {
+      var box = checkboxes[i];
+      var blacklist =  settings[box.id + "Blacklist"];
+      box.addEventListener('click', toggle);
+      box.checked = blacklist.indexOf(tab.url) == -1;
+    }
+ });
+}
+
+function toggle(e) {
+  chrome.tabs.getSelected(null, function(tab) {
+    var blacklist =  settings[e.target.id + "Blacklist"];
+    if(e.target.checked) {
+      var idx = blacklist.indexOf(tab.url);
+      if(idx != -1) blacklist.splice(idx, 1);
+    } 
+    else {
+      blacklist.push(tab.url);
+    }
+    settings[e.target.id + "Blacklist"] = blacklist;
+  });
+  chrome.tabs.executeScript(null, {file:"toggle_" + e.target.id + ".js"});
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('.clicker');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
-  }
+  init();
 });
+
+
+
