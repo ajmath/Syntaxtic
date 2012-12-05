462c3522b9af02d6864ea45aa88b8c9d82bab931
Andrew Matheny <andrew.matheny@listhub.com>
2012-12-04 21:18:44 -0500

    toggle_highlight: Only disable the brush for the theme

--- toggle_highlight.js
+++ toggle_highlight.js
@@ -1,31 +1,11 @@
-function toggle_gutter() {
-  toggle_element('gutter');
-}
 
-function toggle_element(class_name) {
-  var element = document.getElementsByClassName(class_name)[0];
-  if (element.style.display === '')
-  {
-    element.style.display='none';
-  } else
-  {
-    element.style.display='';
-  }
-}
 function toggle_hilight() {
-  toggle_gutter();
-  toggle_element('toolbar');
-  for (var i=0;i<document.styleSheets.length;i++)
-  {
-    if (document.styleSheets[i].disabled)
-    {
-      document.styleSheets[i].disabled = false;
-    } else
-    {
-      document.styleSheets[i].disabled = true;
+  for (var i=0;i<document.styleSheets.length;i++) {
+    var stylesheet = document.styleSheets[i];
+    if (stylesheet.href && stylesheet.href.match(/.*shTheme[a-zA-Z]*\.css/)) {
+      stylesheet.disabled = !stylesheet.disabled;
     }
-
   }
-
 }
+
 toggle_hilight();
